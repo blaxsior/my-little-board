6844458f09e5f26860756bdae465803025107892
blaxsior <heejun2798@gmail.com>
2024-05-26 01:27:01 +0900

    test: mail service 테스트 코드 작성. mailSender / GreenMail 연동 성공

--- src/main/java/com/blaxsior/board/BoardApplication.java
+++ src/main/java/com/blaxsior/board/BoardApplication.java
@@ -1,12 +1,7 @@
 package com.blaxsior.board;
 
-import com.blaxsior.board.domain.SecretConfig;
-import com.blaxsior.board.domain.mail.MailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
--- src/test/java/com/blaxsior/board/domain/mail/MailServiceTest.java
+++ src/test/java/com/blaxsior/board/domain/mail/MailServiceTest.java
@@ -1,4 +1,90 @@
-import static org.junit.jupiter.api.Assertions.*;
-class MailServiceTest {
-  
+package com.blaxsior.board.domain.mail;
+
+
+import com.blaxsior.board.BoardApplication;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import jakarta.mail.MessagingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest
+@SpringBootTest(
+        classes = BoardApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@TestPropertySource(locations = {"classpath:/application-test.properties"})
+//@ContextConfiguration(classes = BoardApplication.class)
+public class MailServiceTest {
+
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("springboot", "springboot"))
+            .withPerMethodLifecycle(false);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    private MailService mailService;
+    @BeforeEach
+    public void beforeEach() {
+        mailService = new MailService(mailSender, templateEngine);
+    }
+
+    @Test
+    void sendEmail() throws MessagingException {
+        var toEmail = "test@test.com";
+        var subject = "test subject";
+        var body = "this is test";
+
+        mailService.sendMail(toEmail, subject, body);
+        greenMail.waitForIncomingEmail(1);
+
+        var messages = greenMail.getReceivedMessages();
+        var message = messages[0];
+        var new_body = GreenMailUtil.getBody(message);
+
+        assertThat(new_body).isEqualTo(body);
+        assertThat(message.getAllRecipients().length).isEqualTo(1);
+        assertThat(message.getSubject()).isEqualTo(subject);
+    }
+
+    @TestConfiguration
+    public static class MailConfiguration {
+        @Bean
+        public JavaMailSender getJavaMailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("127.0.0.1");
+            mailSender.setPort(3025);
+
+            mailSender.setUsername("springboot");
+            mailSender.setPassword("springboot");
+
+            Properties props = mailSender.getJavaMailProperties();
+            props.put("mail.transport.protocol", "smtp");
+
+            return mailSender;
+        }
+    }
 }
